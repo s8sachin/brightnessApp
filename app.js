d3556bdc43f911342d1ed4c3ec6de0d2d56ce5c9
Sachin <s.sachin94@gmail.com>
2017-01-13 14:17:13 +0530

    initial commit changing brightness of screen

---
+++ app.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+const brightness = require('brightness');
+
+const mybrightness = require('./mybrightness.js');
+
+var argv = yargs.argv;
+console.log(argv.myBrightness);
+
+mybrightness.getBrightness();
+mybrightness.changeBrightness(argv.myBrightness);
